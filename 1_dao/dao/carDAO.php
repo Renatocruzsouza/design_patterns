053dc984bd0c928b30bb0eddb484c02916d558fe
Renato <renatosouzacruz263@gmail.com>
2025-10-02 16:53:53 -0300

    feat: implement CarDAO class with create method

--- 1_dao/dao/carDAO.php
+++ 1_dao/dao/carDAO.php
@@ -2,13 +2,29 @@
 
 include_once("models/Car.php");
 
-class CarDAO implements carDAO {
+class CarDAO implements carDAOInterface {
 
+    private $conn;
+
+    public function __construct(PDO $conn) {
+        $this->conn = $conn;
+    }
+    
     public function findAll(){
 
     }
 
     public function create(Car $car) {
+
+        $stmt = $this->conn->prepare("INSERT INTO cars (brand, km, color) VALUES (:brand, :km, :color)");
+
+        $stmt->bindParam(":brand", $car->getBrand());
+
+        $stmt->bindParam(":km", $car->getKm());
+
+        $stmt->bindParam(":color", $car->getColor());
+
+        $stmt->execute();
 
     }
 }
